a7223d10d1a381ca760579bd91572b928c327262
Irakli gachechiladze <boomikaboom@gmail.com>
2022-09-23 18:15:46 +0400

    second

---
+++ Day_2/lessson2.py
@@ -0,0 +1,7 @@
+
+
+name = "Irakli"
+age = 37
+height = 183
+
+print("chemi saxelia {} chemi asakia {} chemi simaglea {} ".format(name, age, height))
---
+++ Day_3/lesson3.py
@@ -0,0 +1,2 @@
+full_name= "irakli gchechiladze"
+print(full_name[4:9])
